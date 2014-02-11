b985ba1cee6f59ff3e69bf28e2f9a28111276553
AngelaRigden <angelarigden@231-6-41-155-wireles1x.bu.edu>
2014-02-11 15:27:21 -0500

    Made script Aggregate_Met_Data. Hooray.

---
+++ Aggregate_Met_Data.R
@@ -0,0 +1,21 @@
+# Aggregate the met data
+
+# LOAD MASTER DATA FILE
+master_data <- read.csv("data_file_011314_point.csv")
+
+# LOAD UNIQUE DATA FILE (POINT)
+unique_data <- read.csv("PointMetData.csv")
+
+for rows in master data file {
+# Find ID
+# load met data
+# aggregate
+
+# write 30, 60, 90 month PP
+# write 30, 60, 90 temp data
+
+# write photoperiod
+
+}
+
+# Save new master plus met csv
