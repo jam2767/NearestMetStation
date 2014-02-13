43cea70da894cd836bfa4cffbd8a529a87eed217
Angela Rigden <ajrigden@bu.edu>
2014-02-13 11:26:33 -0500

    Lunch time.

--- Aggregate_Met_Data.R
+++ Aggregate_Met_Data.R
@@ -2,6 +2,10 @@
 
 # LOAD MASTER DATA FILE
 master_data <- read.csv("data_file_013014_point_edit2.csv")
+# Replace little "na"s with NA:
+small_na = (master_data=="na")
+master_data[small_na] = NA
+
 master_data$Site_ID = NA
 master_data$Met_Station_ID = NA
 
@@ -28,11 +32,38 @@ for (ROW in 1:nrow(unique_data)){
 
 }
 
+# Okay, noy I think we should reshape the master data file so that each
+# measurement has its own row as well as a new column for the type of
+# measurement, one of {LeafFall50, LeafFall80, LeafFall100, LAI_zero}
+#
+# Let's kick this pig!
+
+# First, find all of the records that have LeafFall50:
+has_LF50 <- !is.na(master_data$LeafFall50)
+LF50_data <- subset(master_data[has_LF50,])
+
+# Now for LeafFall80:
+has_LF80 <- !is.na(master_data$LeafFall80)
+LF80_data <- subset(master_data[has_LF80,])
+
+# Now for LeafFall100:
+has_LF100 <- !is.na(master_data$LeafFall100)
+LF100_data <- subset(master_data[has_LF100,])
+
+# Now for LAI_zero:
+has_LAI_zero <- !is.na(master_data$LAI_zero)
+LAI_zero_data <- subset(master_data[has_LAI_zero,])
+
 
 for ROW in 1:nrow(master_data) {
 
   # Find met station ID
   met_stat_id = master_data$Met_Station_ID[ROW]
+  
+  # Angela -- I think we should for now just use the columns LeafFall50, 
+  # LeafFall80, LeafFall100, and LAI_zero. If we should include other values
+  # they should be put into those columns (by someone else)
+  
   
   # Find DOY of phenology 
   all_doy_phen = master_data[ROW,23:64]
